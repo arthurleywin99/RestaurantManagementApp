4e44d8b0a56ffff80e921c40e466047345de348d
Bùi Giang <truonggiang.1112@icloud.com>
2021-10-30 14:55:01 +0700

    Done

--- RestaurantManagementApp/GUI/Report_PopupScreen.Designer.cs
+++ RestaurantManagementApp/GUI/Report_PopupScreen.Designer.cs
@@ -30,28 +30,107 @@ namespace RestaurantManagementApp.GUI
         private void InitializeComponent()
         {
             this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.ControlBar = new System.Windows.Forms.TableLayoutPanel();
+            this.btnMinimized = new System.Windows.Forms.PictureBox();
+            this.btnMaximized = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.PictureBox();
+            this.ControlBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaximized)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer
             // 
-            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.reportViewer.LocalReport.ReportEmbeddedResource = "RestaurantManagementApp.Report.InvoicesReport.rdlc";
-            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Location = new System.Drawing.Point(0, 33);
             this.reportViewer.Name = "reportViewer";
             this.reportViewer.ServerReport.BearerToken = null;
-            this.reportViewer.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer.Size = new System.Drawing.Size(800, 417);
             this.reportViewer.TabIndex = 0;
+            // 
+            // ControlBar
+            // 
+            this.ControlBar.BackColor = System.Drawing.Color.Transparent;
+            this.ControlBar.ColumnCount = 7;
+            this.ControlBar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ControlBar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1F));
+            this.ControlBar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.ControlBar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1F));
+            this.ControlBar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.ControlBar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1F));
+            this.ControlBar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.ControlBar.Controls.Add(this.btnMinimized, 2, 0);
+            this.ControlBar.Controls.Add(this.btnMaximized, 4, 0);
+            this.ControlBar.Controls.Add(this.btnExit, 6, 0);
+            this.ControlBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ControlBar.Location = new System.Drawing.Point(0, 0);
+            this.ControlBar.Margin = new System.Windows.Forms.Padding(0);
+            this.ControlBar.Name = "ControlBar";
+            this.ControlBar.RowCount = 1;
+            this.ControlBar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.ControlBar.Size = new System.Drawing.Size(800, 36);
+            this.ControlBar.TabIndex = 9;
+            this.ControlBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ControlBar_MouseDown);
+            // 
+            // btnMinimized
+            // 
+            this.btnMinimized.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimized.Image = global::RestaurantManagementApp.Properties.Resources.MinimizeButton;
+            this.btnMinimized.Location = new System.Drawing.Point(693, 3);
+            this.btnMinimized.Name = "btnMinimized";
+            this.btnMinimized.Size = new System.Drawing.Size(30, 30);
+            this.btnMinimized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMinimized.TabIndex = 1;
+            this.btnMinimized.TabStop = false;
+            this.btnMinimized.Click += new System.EventHandler(this.btnMinimized_Click);
+            // 
+            // btnMaximized
+            // 
+            this.btnMaximized.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximized.Image = global::RestaurantManagementApp.Properties.Resources.MaximizeButton;
+            this.btnMaximized.Location = new System.Drawing.Point(730, 3);
+            this.btnMaximized.Name = "btnMaximized";
+            this.btnMaximized.Size = new System.Drawing.Size(30, 30);
+            this.btnMaximized.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMaximized.TabIndex = 1;
+            this.btnMaximized.TabStop = false;
+            this.btnMaximized.Click += new System.EventHandler(this.btnMaximized_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Image = global::RestaurantManagementApp.Properties.Resources.CloseButton;
+            this.btnExit.Location = new System.Drawing.Point(767, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(30, 30);
+            this.btnExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnExit.TabIndex = 0;
+            this.btnExit.TabStop = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // Report_PopupScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ControlBar);
             this.Controls.Add(this.reportViewer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Report_PopupScreen";
             this.Text = "Report_PopupScreen";
             this.Load += new System.EventHandler(this.Report_PopupScreen_Load);
+            this.ControlBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimized)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaximized)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +138,9 @@ namespace RestaurantManagementApp.GUI
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private System.Windows.Forms.TableLayoutPanel ControlBar;
+        private System.Windows.Forms.PictureBox btnMinimized;
+        private System.Windows.Forms.PictureBox btnMaximized;
+        private System.Windows.Forms.PictureBox btnExit;
     }
 }
--- RestaurantManagementApp/GUI/Report_PopupScreen.cs
+++ RestaurantManagementApp/GUI/Report_PopupScreen.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RestaurantManagementApp.BusinessTier;
+using RestaurantManagementApp.UtilityMethod;
 
 namespace RestaurantManagementApp.GUI
 {
@@ -16,11 +18,45 @@ namespace RestaurantManagementApp.GUI
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
+            Region = Region.FromHrgn(Utility.CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
         }
 
         private void Report_PopupScreen_Load(object sender, EventArgs e)
         {
             this.reportViewer.RefreshReport();
+        }
+
+        private void btnMinimized_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMaximized_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Normal)
+            {
+                WindowState = FormWindowState.Maximized;
+                Region = Region.FromHrgn(Utility.CreateRoundRectRgn(0, 0, Width, Height, 0, 0));
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+                Region = Region.FromHrgn(Utility.CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ControlBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Utility.ReleaseCapture();
+                Utility.SendMessage(Handle, 0x112, 0xf012, 0);
+            }
         }
     }
 }
